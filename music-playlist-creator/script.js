abf4dcf66388ebe57155e092af3419e388819da5
Isabella Marin <isabellamarin@meta.com>
2025-06-03 22:44:31 -0700

    feat: completed milestone 1 and 2, except for bug with modal

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,20 @@
+// JavaScript for Opening and Closing the Modal
+const modal = document.getElementById("modal-overlay");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+   document.getElementById('playlistTitle').innerText = playlist.name;
+   document.getElementById('playlistImage').src = playlist.imageUrl;
+   document.getElementById('creatorName').innerText = `Creator Name: ${playlist.creator}`;
+   document.getElementById('songList').innerHTML = `<strong>List of Songs:</strong> ${playlist.songs.join(', ')}`;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
