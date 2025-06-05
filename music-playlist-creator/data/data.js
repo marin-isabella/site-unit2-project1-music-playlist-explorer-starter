7d3d45e7cbbbd9601f67dfd46197008b5805a7df
Isabella Marin <isabellamarin@meta.com>
2025-06-04 23:10:22 -0700

    feat: completed M3 with the exception of error handling if data.js is empty

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,74 @@
+const playlists = [
+    {
+		"playlistId": 123456789,
+		"playlist_name": "My Favorites",
+		"playlist_author": "Isabella Marin",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["Where You Are", "Sailor Song", "Vanish Into You", "blue", "Ocean"],
+        "like": 0
+	},
+    {
+		"playlistId": 987654321,
+		"playlist_name": "Summer Vibes",
+		"playlist_author": "Liam Chen",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["Sunshine", "Beachin'", "Cheeseburger in Paradise", "Summertime", "School's Out"],
+        "like": 0
+	},
+    {
+		"playlistId": 111222333,
+		"playlist_name": "Workout Mix",
+		"playlist_author": "Ava Lee",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["Eye of the Tiger", "Uptown Funk", "Can't Stop", "We Will Rock You", "Unstoppable"],
+        "like": 0
+	},
+    {
+		"playlistId": 444555666,
+		"playlist_name": "Relaxation Station",
+		"playlist_author": "Ethan Kim",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["Weightless", "Clair de Lune", "River Flows in You", "Hallelujah", "Stay with Me"],
+        "like": 0
+	},
+    {
+		"playlistId": 777888999,
+		"playlist_name": "Throwback Thursday",
+		"playlist_author": "Mia Patel",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["I Want It That Way", "Bye Bye Bye", "Wannabe", "No Scrubs", "Waterfalls"],
+        "like": 0
+	},
+    {
+		"playlistId": 101112131,
+		"playlist_name": "Focus Flow",
+		"playlist_author": "Julian Sanchez",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["Pompeii", "Best Day of My Life", "On Top of the World", "Unwritten", "Brave"],
+        "like": 0
+	},
+    {
+		"playlistId": 141516171,
+		"playlist_name": "Gaming Soundtrack",
+		"playlist_author": "Sophia Rodriguez",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["The Legend of Zelda", "Super Mario Bros.", "Halo", "The Elder Scrolls V", "World of Warcraft"],
+        "like": 0
+	},
+    {
+		"playlistId": 181920212,
+		"playlist_name": "Love Songs",
+		"playlist_author": "Alexander Martin",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["I Will Always Love You", "My Heart Will Go On", "Landslide", "Perfect", "A Thousand Years"],
+        "like": 0
+	},
+    {
+		"playlistId": 222324252,
+		"playlist_name": "Dance Party",
+		"playlist_author": "Gabriella Brown",
+		"playlist_art": "./assets/img/playlist.png",
+		"songs": ["Uptown Funk", "Can't Stop the Feeling!", "Happy", "Celebration", "Dynamite"],
+        "like": 0
+    }
+]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,3 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+   loadPlaylists();
+})
+
+function loadPlaylists() {
+   console.log("playlists loaded!");
+   const playlistContainer = document.querySelector(".playlist-cards");
+   console.log("hi");
+   playlists.forEach((playlist) => {
+      console.log("meow");
+      console.log(playlist);
+      const element = createPlaylistsElement(playlist);
+      console.log(element);
+      playlistContainer.appendChild(element);
+   })
+}
+
+function createPlaylistsElement(playlist) {
+   console.log("createPlaylistsElement called!")
+   const div = document.createElement('div');
+   div.className = 'card';
+   div.innerHTML = `
+      <img src=${playlist.playlist_art} alt="song image" width="200">
+       <h3>${playlist.playlist_name}</h3>
+       <p>${playlist.playlist_author}</p>
+       <button>Likes: ${playlist.like}</button>
+   `;
+   return div;
+}
+
 // JavaScript for Opening and Closing the Modal
 // const modal = document.getElementById("modal-overlay");
 // const span = document.getElementsByClassName("close")[0];
