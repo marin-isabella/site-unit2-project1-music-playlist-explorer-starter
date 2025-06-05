74f7aecbcce3e45d26b855ec3d21862b212a6f52
Isabella Marin <isabellamarin@meta.com>
2025-06-04 21:24:01 -0700

    feat: created data.js file with mock data

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,21 +1,21 @@
 // JavaScript for Opening and Closing the Modal
-const modal = document.getElementById("modal-overlay");
-const span = document.getElementsByClassName("close")[0];
+// const modal = document.getElementById("modal-overlay");
+// const span = document.getElementsByClassName("close")[0];
 
-function openModal(playlist) {
-   console.log("hi");
-   document.getElementById('playlistTitle').innerText = playlist.name;
-   document.getElementById('playlistImage').src = playlist.imageUrl;
-   document.getElementById('creatorName').innerText = `Creator Name: ${playlist.creator}`;
-   document.getElementById('songList').innerHTML = `<strong>List of Songs:</strong> ${playlist.songs.join(', ')}`;
-   modal.style.display = "block";
-}
+// function openModal(playlist) {
+//    console.log("hi");
+//    document.getElementById('playlistTitle').innerText = playlist.name;
+//    document.getElementById('playlistImage').src = playlist.imageUrl;
+//    document.getElementById('creatorName').innerText = `Creator Name: ${playlist.creator}`;
+//    document.getElementById('songList').innerHTML = `<strong>List of Songs:</strong> ${playlist.songs.join(', ')}`;
+//    modal.style.display = "block";
+// }
 
-span.onclick = function() {
-   modal.style.display = "none";
-}
-window.onclick = function(event) {
-   if (event.target == modal) {
-      modal.style.display = "none";
-   }
-}
+// span.onclick = function() {
+//    modal.style.display = "none";
+// }
+// window.onclick = function(event) {
+//    if (event.target == modal) {
+//       modal.style.display = "none";
+//    }
+// }
