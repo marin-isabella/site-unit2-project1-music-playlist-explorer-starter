73e189b9e3b41c3175c040fa50499b52dc065881
Isabella Marin <isabellamarin@meta.com>
2025-06-05 22:36:19 -0700

    feat: finished implementing shuffle functionality and styling shuffle button

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -3,7 +3,10 @@ const modal = document.getElementById("modal-overlay");
 const span = document.getElementsByClassName("close")[0];
 
 span.addEventListener('click', closeModal);
+
+let openPlaylist = null;
 function openModal(playlist) {
+   openPlaylist = playlist;
    document.getElementById('playlist-title').innerText = playlist.playlist_name;
    document.getElementById('playlist-image').src = playlist.playlist_art;
    document.getElementById('creator-name').innerText = `Created by ${playlist.playlist_author}`;
@@ -13,22 +16,7 @@ function openModal(playlist) {
 
    const songsContainer = document.createElement('div');
    songsContainer.classList.add('songs-container');
-
-   playlist.songs.forEach(song => {
-      const songElement = document.createElement('div');
-      songElement.classList.add('song');
-      songElement.innerHTML = `
-         <img src="${song.song_cover}" alt="${song.title} cover" width="50">
-         <div class="song-info">
-            <h4>${song.title}</h4>
-            <p>${song.artist}</p>
-            <p>${song.duration}</p>
-         </div>
-      `;
-      songsContainer.appendChild(songElement);
-   });
-
-   songListElement.appendChild(songsContainer);
+   makeSongsList(playlist.songs);
 
    modal.style.display = "block";
 }
@@ -40,6 +28,52 @@ function closeModal() {
 document.addEventListener('DOMContentLoaded', () => {
    loadPlaylists();
 })
+
+function makeSongsList(songs) {
+   const songListElement = document.getElementById('song-list');
+   songListElement.innerHTML = '<h3>Songs:</h3>';
+
+   const songsContainer = document.createElement('div');
+   songsContainer.classList.add('songs-container');
+
+   songs.forEach(song => {
+      const songElement = document.createElement('div');
+      songElement.classList.add('song');
+      songElement.innerHTML = `
+         <img src="${song.song_cover}" alt="${song.title} cover" width="50">
+         <div class="song-info">
+            <h4>${song.title}</h4>
+            <p>${song.artist}</p>
+            <p>${song.duration}</p>
+         </div>`;
+      songsContainer.appendChild(songElement);
+   });
+   songListElement.appendChild(songsContainer);
+}
+
+function shuffleSongs(arr) {
+   // Copy of array
+   const shuffledArr = [...arr];
+
+   // Fisher-Yates shuffle algorithm
+   // Reference: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+   for (let i = shuffledArr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = shuffledArr[i];
+      shuffledArr[i] = shuffledArr[j];
+      shuffledArr[j] = temp;
+   }
+
+   return shuffledArr;
+}
+
+const shuffleSongsButton = document.getElementById('shuffle-button');
+shuffleSongsButton.addEventListener('click', () => {
+   if (openPlaylist) {
+      const shuffledSongs = shuffleSongs(openPlaylist.songs);
+      makeSongsList(shuffledSongs);
+   }
+});
 
 window.addEventListener('click', (e) => {
    if (e.target == modal) {
